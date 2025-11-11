a9937f56fa74902f2421bfb45ca4e05c3109affa
ivannakotyk <ivannakotik3@gmail.com>
2025-11-11 07:34:58 +0200

    fixing a branch conflict

--- src/main/java/ua/kpi/ivanka/marketplace/service/ProductService.java
+++ src/main/java/ua/kpi/ivanka/marketplace/service/ProductService.java
@@ -4,18 +4,18 @@ import ua.kpi.ivanka.marketplace.dto.ProductDTO;
 import ua.kpi.ivanka.marketplace.dto.request.ProductCreateDTO;
 import ua.kpi.ivanka.marketplace.dto.request.ProductUpdateDTO;
 
+
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
+
 public interface ProductService {
-
     ProductDTO createProduct(ProductCreateDTO dto);
-
     List<ProductDTO> listProducts();
-
     ProductDTO getProduct(UUID id);
-
     ProductDTO updateProduct(UUID id, ProductUpdateDTO dto);
-
     void deleteProduct(UUID id);
+    Map<String, Object> getRates();
+
 }
